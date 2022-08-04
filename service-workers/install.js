0d7e1dde36c1570fd452d93969a3624647b9bf8a
Daniela Leloutre <daniela@woowup.com>
2022-08-04 17:32:02 -0300

    quito un archivo

--- service-workers/install.js
+++
@@ -1,7 +0,0 @@
-function beams() {
-    importScripts("https://js.pusher.com/beams/service-worker.js");
-    // eslint-disable-next-line no-console
-    console.log("Pusher Beams - Service Worker installed")
-}
-
-export default beams;
