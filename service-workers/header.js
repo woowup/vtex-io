e3907bf39d8436fcfbdf6b910c05578b5e920338
r-cbl <ezequiel@woowup.com>
2023-04-24 15:53:42 -0300

    cambios en pr

--- service-workers/header.js
+++ service-workers/header.js
@@ -1,5 +1,5 @@
 function wuHeaderSW() {
-    importScripts("https://js.pusher.com/beams/service-worker.js");
+    importScripts("https://js.pusher.com/7.0/pusher.worker.min.js");
     // eslint-disable-next-line no-console
     console.log('[WU] - Activado el Service Worker de Beams')
 }
